4bc37052291304fef7a7fa11cae69e0215ec305a
Wesley Araujo <wesleyaraujopi@gmail.com>
2015-11-14 09:18:39 -0300

    Alterando o texto do banner

---
+++ principal.js
@@ -0,0 +1,8 @@
+var banners = ["Do lixo ao luxo!","Reaproveitar é aproveitar!"];
+var bannerAtual = 0;
+function trocaBanner() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector(’h2#mensagem’).textContent =
+	banners[bannerAtual];
+}
+setInterval(trocaBanner, 1000);
